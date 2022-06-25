133771bda5fb8600bab930c2cae5284cd2adfb1d
Antony Prince J <antony.prince@thoughtworks.com>
2022-06-25 09:24:44 +0530

    [Antony] Add test for repository fetching all todos

--- src/test/java/com/tw/todo/repository/TodoRepositoryTest.java
+++ src/test/java/com/tw/todo/repository/TodoRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -26,4 +28,18 @@ public class TodoRepositoryTest {
         assertThat(savedTodo.isCompleted(), is(false));
     }
 
+    @Test
+    void shouldFetchAllTodos() {
+        Todo todoOne = new Todo("NEEV", false);
+        Todo todoTwo = new Todo("TWARAN", true);
+        todoRepository.save(todoOne);
+        todoRepository.save(todoTwo);
+
+        List<Todo> fetchedTodos = todoRepository.findAll();
+
+        assertThat(fetchedTodos.get(0).getDescription(), is("NEEV"));
+        assertThat(fetchedTodos.get(0).isCompleted(), is(false));
+        assertThat(fetchedTodos.get(1).getDescription(), is("TWARAN"));
+        assertThat(fetchedTodos.get(1).isCompleted(), is(true));
+    }
 }
